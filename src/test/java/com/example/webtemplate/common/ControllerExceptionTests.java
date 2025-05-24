a65fc62a5ef86d45aa142b39412c0f488743f914
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-05-24 19:45:24 +0900

    ✅ Test : 404 exception test

---
+++ src/test/java/com/example/webtemplate/common/ControllerExceptionTests.java
@@ -0,0 +1,25 @@
+package com.example.webtemplate.common;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerExceptionTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getNonExistentEntity_shouldReturn404() throws Exception {
+        var request = get("/api/accounts/0").accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(status().isNotFound());
+    }
+}
