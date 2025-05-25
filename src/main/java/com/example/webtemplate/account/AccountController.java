db4bffde848731f8c8d46cd899fcfeede29206d9
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-05-25 17:13:39 +0900

    🚧 WIP : init controller

--- src/main/java/com/example/webtemplate/account/AccountController.java
+++ src/main/java/com/example/webtemplate/account/AccountController.java
@@ -1,21 +1,15 @@
 package com.example.webtemplate.account;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import java.util.List;
 
-import org.springframework.hateoas.CollectionModel;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.webtemplate.account.dto.AccountPatchPasswordResponse;
 
 @RestController
 class AccountController {
@@ -26,40 +20,25 @@ class AccountController {
     this.service = service;
   }
 
-  @GetMapping("/accounts")
-  CollectionModel<EntityModel<Account>> all() {
-
-    List<EntityModel<Account>> accounts = service.getAllAccounts().stream()
-        .map(employee -> EntityModel.of(employee,
-            linkTo(methodOn(AccountController.class).one(employee.getId())).withSelfRel(),
-            linkTo(methodOn(AccountController.class).all()).withRel("accounts")))
-        .toList();
-
-    return CollectionModel.of(accounts,
-        linkTo(methodOn(AccountController.class).all()).withSelfRel());
-  }
-
   @PostMapping("/accounts")
-  Account newEmployee(@RequestBody Account newEmployee) {
-    return service.createAccount(newEmployee);
+  EntityModel<Account> register(@RequestBody Account newAccount) {
+    throw new NotImplementedException();
   }
 
-  @GetMapping("/accounts/{id}")
-  EntityModel<Account> one(@PathVariable("id") Long id) {
-    Account employee = service.findById(id);
-    return EntityModel.of(employee, linkTo(methodOn(AccountController.class).one(id)).withSelfRel(),
-        linkTo(methodOn(AccountController.class).all()).withRel("accounts"));
+  @DeleteMapping("/accounts/me")
+  EntityModel<Account> signOut() {
+    // Assuming the current user's ID is given by a security context
+    throw new NotImplementedException();
   }
 
-  @PatchMapping("/accounts/{id}/password")
-  AccountPatchPasswordResponse patchPassword(@PathVariable("id") Long id,
-      @RequestBody Account reqBody) {
-    service.patchAccountPassword(id, reqBody.getPassword());
-    return new AccountPatchPasswordResponse(new java.util.Date());
+  @PatchMapping("/accounts/me/password")
+  EntityModel<Account> changePassword(@RequestBody Account reqBody) {
+    // Assuming the current user's ID is given by a security context
+    throw new NotImplementedException();
   }
 
-  @DeleteMapping("/accounts/{id}")
-  void deleteEmployee(@PathVariable("id") Long id) {
-    service.deleteById(id);
+  @GetMapping("/accounts/index")
+  List<Integer> accountIndex() {
+    throw new NotImplementedException();
   }
 }
