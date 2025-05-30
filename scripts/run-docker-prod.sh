bc4664921492437eb960c388cdd6bdf1bb006ac5
Ji-eun Yang <jidak1165@gmail.com>
2025-05-30 18:48:23 +0900

    :adhesive_bandage: 불필요한 설정 제거

--- scripts/run-docker-prod.sh
+++ scripts/run-docker-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 루트 디렉토리에서 실행
 
-echo "🧼 운영 컨테이너 정리 중... (로컬 이미지도 삭제)"
-docker compose --env-file .env.prod -f docker-compose.prod.yaml down --rmi local --remove-orphans
+echo "🧼 운영 컨테이너 정리 중..."
+docker compose --env-file .env.prod -f docker-compose.prod.yaml down --remove-orphans
 
 echo "🚀 운영 컨테이너 실행 중..."
 docker compose --env-file .env.prod -f docker-compose.prod.yaml up --build -d
