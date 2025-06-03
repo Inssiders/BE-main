fa276f465158613078b0fbe1b72ef8f87c2cad39
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-06-04 08:06:41 +0900

    🔥 Remove : reinit packages

--- src/main/java/com/example/webtemplate/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webtemplate.account;
-
-import com.example.webtemplate.common.repository.SoftDeleteRepository;
-
-interface AccountRepository extends SoftDeleteRepository<Account, Long> {
-
-}
