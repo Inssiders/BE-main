4d9eb3dd1c166fca7b4bff65aca7f0451a51d90b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-06-08 12:22:56 +0900

    🐛 Fix : apply renewal authorization

--- src/main/java/com/inssider/api/domains/profile/UserProfileRequestsDto.java
+++ src/main/java/com/inssider/api/domains/profile/UserProfileRequestsDto.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 public class UserProfileRequestsDto {
   public record UpdateProfile(
-      Long id,
       Optional<String> nickname,
       Optional<String> profileUrl,
       Optional<String> bio,
