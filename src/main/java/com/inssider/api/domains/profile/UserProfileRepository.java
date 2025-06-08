bd3cfc7e0bf47ee8a2eb1d8f8686065b7883080f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-06-09 03:40:08 +0900

    🐛 Fix : simplify query using `Pageable`

--- src/main/java/com/inssider/api/domains/profile/UserProfileRepository.java
+++ src/main/java/com/inssider/api/domains/profile/UserProfileRepository.java
@@ -1,5 +1,9 @@
 package com.inssider.api.domains.profile;
 
 import com.inssider.api.common.repository.SoftDeleteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-interface UserProfileRepository extends SoftDeleteRepository<UserProfile, Long> {}
+interface UserProfileRepository extends SoftDeleteRepository<UserProfile, Long> {
+  Page<UserProfile> findByNicknameContainingIgnoreCase(String nickname, Pageable pageable);
+}
