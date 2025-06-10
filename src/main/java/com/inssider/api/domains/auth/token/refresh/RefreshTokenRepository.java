933a95cb3fe8bc36fff290a72d9262b87b86762e
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-06-10 16:36:27 +0900

    🔄 Refactor : package structure (removed)

--- src/main/java/com/inssider/api/domains/auth/token/refresh/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inssider.api.domains.auth.token.refresh;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {}
--- src/test/java/com/inssider/api/domains/account/TestAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inssider.api.domains.account;
-
-import org.springframework.boot.test.context.TestComponent;
-import org.springframework.context.annotation.Primary;
-
-@TestComponent
-@Primary
-public interface TestAccountRepository extends AccountRepository {}
