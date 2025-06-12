81c87399162561d5528767e1e0a3cda2b1dbe4ee
Ji-eun Yang <jidak1165@gmail.com>
2025-06-12 22:53:27 +0900

    :truck: 테스트 유저 생성을 위한 파일 수정

--- scripts/run-docker-dev.sh
+++ scripts/run-docker-dev.sh
@@ -2,9 +2,9 @@
 # 스크립트는 항상 루트에서 실행
 
 echo "🧼 기존 컨테이너 정리 중..."
-docker compose --env-file .env.dev -f docker-compose.dev.yaml down --volumes --remove-orphans
+docker compose --env-file .env -f docker-compose.dev.yaml down --volumes --remove-orphans
 
 echo "🚀 컨테이너 실행 중..."
-docker compose --env-file .env.dev -f docker-compose.dev.yaml up -d
+docker compose --env-file .env -f docker-compose.dev.yaml up -d
 
 echo "✅ 개발 환경 실행 완료!"
--- src/main/java/com/inssider/api/domains/account/AccountInitializer.java
+++ src/main/java/com/inssider/api/domains/account/AccountInitializer.java
@@ -4,23 +4,35 @@ import com.inssider.api.common.Util;
 import jakarta.annotation.PostConstruct;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Log4j2
 @Configuration
 @RequiredArgsConstructor
-@Profile("load")
+@Profile("dev")
 class AccountInitializer {
   // reference to repository and add some entities to the database
 
+  @Autowired
   private AccountRepository accountRepository;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   private Supplier<Account> accountSupplier =
-      () -> accountRepository.save(Util.accountGenerator().get());
+      () -> {
+        Account account = Util.accountGenerator().get();
+        log.info("Password {}", account.getPassword());
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        return accountRepository.save(account);
+      };
 
   @PostConstruct
   public void init() {
     var account = accountSupplier.get();
-    accountRepository.softDelete(account.getId());
+//    accountRepository.softDelete(account.getId());
   }
 }
